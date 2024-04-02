56b1a5c05b1a928771eb7f08b7bd2b6cb1d3fe33
Nikhil Patel <nikhil.patel@capillarytech.com>
2024-04-02 11:14:23 +0530

    mfe expose

--- mfe-exposed-components.js
+++ mfe-exposed-components.js
@@ -2,8 +2,8 @@
 // Contents of this file are used in webpack config's ModuleFederationPlugin.
 //
 module.exports = {
-    // which exposes
-    // ADD MFE COMPONENT YOU WANT TO EXPORT example:
-    // './Home': './app/components/pages/Home',
-  };
-  
+  // which exposes
+  // ADD MFE COMPONENT YOU WANT TO EXPORT example:
+  './LoyaltyTagsPage': '/app/components/pages/LoyaltyTags',
+  './ExtendedFieldsPage': '/app/components/pages/ExtFields',
+};
