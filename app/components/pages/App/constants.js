9ffc291c3a84bbd4f973e031234911acd8c6e17c
Nikhil Patel <nikhil.patel@capillarytech.com>
2024-04-09 18:41:32 +0530

    redirection fix

--- app/components/pages/App/constants.js
+++ app/components/pages/App/constants.js
@@ -5,7 +5,3 @@ export const FAILURE = 'FAILURE';
 export const NATIVE = 'native';
 export const GLOBAL = 'global';
 export const EXTERNAL = 'external';
-
-export const LAST_VISITED_PATH = '_last_visited_path';
-export const HISTORY_PATH = '_navigation_history';
-export const INDEX_PATH = '/index.html';
